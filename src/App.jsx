802e957e7765594389d9f10bc2713bb1db4384e7
Sahil Sahu <sahilsahu731709@gmail.com>
2024-10-29 12:57:30 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Main from './components/Main'
+import Side from './components/Side'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Side />
+    <Main/>
+    </>
+  )
+}
+
+export default App
